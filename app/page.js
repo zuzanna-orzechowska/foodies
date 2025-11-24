0ba43f703316af87242794e9fb6208bdce869601
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-11-24 10:53:35 +0100

    initial commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Laboratorium nr 11 - projekt startowy
+      </h1>
+    </main>
+  );
+}
