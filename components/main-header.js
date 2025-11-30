d2806e78a91593cc1d989fa307c17aaff1f92e61
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-11-30 13:51:28 +0100

    optymalizacja obrazka i dodanie komponentu main-header-background

--- components/main-header.js
+++ components/main-header.js
@@ -2,26 +2,31 @@ import React from 'react';
 import Link from 'next/link';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css' 
+import Image from 'next/image';
+import MainHeaderBackground from './main-header-background';
 
 function MainHeader() {
   return (
-    <header className={classes.header}>
-        <Link href='/' className={classes.logo}>
-            <img src={logoImg.src} alt='A plate with food on it' />
-            NextLevel Food
-        </Link>
+    <>
+        <MainHeaderBackground/>
+        <header className={classes.header}>
+            <Link href='/' className={classes.logo}>
+                <Image src={logoImg.src} width={500} height={500} alt='A plate with food on it' />
+                NextLevel Food
+            </Link>
 
-        <nav className={classes.nav}>
-            <ul>
-                <li>
-                    <Link href='/meals'>Browse Meals</Link>
-                </li>
-                <li>
-                    <Link href='/community'>Foodies Community</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href='/meals'>Browse Meals</Link>
+                    </li>
+                    <li>
+                        <Link href='/community'>Foodies Community</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>
   )
 }
 
