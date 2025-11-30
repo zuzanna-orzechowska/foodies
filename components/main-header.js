8de58883754a17e94909d4f7b179723c1bf16bbb
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-11-30 13:28:04 +0100

    stworzenie i stylizacja headera

---
+++ components/main-header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css' 
+
+function MainHeader() {
+  return (
+    <header className={classes.header}>
+        <Link href='/' className={classes.logo}>
+            <img src={logoImg.src} alt='A plate with food on it' />
+            NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+            <ul>
+                <li>
+                    <Link href='/meals'>Browse Meals</Link>
+                </li>
+                <li>
+                    <Link href='/community'>Foodies Community</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default MainHeader
