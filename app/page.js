cd7a900caffe6662cf8e39cc0503ee377d1244d1
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-12-01 11:45:19 +0100

    uzupełnienie treści na stronie głownej / utworzenie i style dla strony meals

--- app/page.js
+++ app/page.js
@@ -23,15 +23,29 @@ export default function Home() {
       </header>
       <main>
         <section className={classes.section}>
-          <h2>Naglowek do uzupelnienia</h2>
-          <p>sss</p>
-          <p>ssss</p>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to 
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
         </section>
 
         <section className={classes.section}>
-          <h2>Naglowke do uzupelnienia</h2>
-          <p>dsfdsfsdf</p>
-          <p>sdsfsdfd</p>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to 
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
         </section>
       </main>
     </>
