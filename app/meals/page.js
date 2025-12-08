70df09d1e696c8c7d9bebb92078ced5846207c16
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-12-08 18:25:03 +0100

    próba naprawy nr1

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,28 +1,54 @@
-import React, { Suspense } from 'react'
+// import React, { Suspense } from 'react'
+// import classes from './page.module.css'
+// import Link from 'next/link'
+// import MealsGrid from '@/components/(meals)/meals-grid'
+// import { getMeals } from '@/lib/meals';
+
+// async function Meals () {
+//   const meals = await getMeals();
+//   return <MealsGrid meals={meals}></MealsGrid>
+// }
+
+// export default function MealsMain() {
+//   return (
+//     <>
+//       <header className={classes.header}>
+//         <h1>Delicious meals, created{' '} <span className={classes.highlight}>by you</span></h1>
+//         <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+//         <p className={classes.cta}>
+//           <Link href="/meals/share"> Share you favorite recipe!</Link>
+//         </p>
+//       </header>
+//       <main className={classes.main}>
+//           <Meals/>
+//         {/* <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+//         </Suspense> */}
+//       </main>
+//     </>
+//   )
+// }
 import classes from './page.module.css'
 import Link from 'next/link'
 import MealsGrid from '@/components/(meals)/meals-grid'
-import { getMeals } from '@/lib/meals';
+import { getMeals } from '@/lib/meals'
 
-async function Meals () {
-  const meals = await getMeals();
-  return <MealsGrid meals={meals}></MealsGrid>
-}
+export default async function MealsMain() {
+  const meals = await getMeals()
 
-export default function MealsMain() {
   return (
     <>
       <header className={classes.header}>
-        <h1>Delicious meals, created{' '} <span className={classes.highlight}>by you</span></h1>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
         <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
         <p className={classes.cta}>
-          <Link href="/meals/share"> Share you favorite recipe!</Link>
+          <Link href="/meals/share">Share your favorite recipe!</Link>
         </p>
       </header>
       <main className={classes.main}>
-        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
-          <Meals/>
-        </Suspense>
+        <MealsGrid meals={meals} />
       </main>
     </>
   )
