50bbb72ffbf2ddf5db11c18f9e5766bb560bbd72
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-12-08 11:35:23 +0100

    utworzenie loading page oraz uzupełnienie strony szczegółowej dla dań

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import classes from './page.module.css';
+import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
 
-export default function page({params}) {
-    const nazwa = params.someName;
+async function Meal({ params }) {
+  const meal = await getMeal(params.mealSlug);
+
   return (
-    <div>Szczegóły na temat: {nazwa}</div>
-  )
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+
+      <div className={classes.instructions}>
+        <p>{meal.instructions}</p>
+      </div>
+    </>
+  );
+}
+
+export default function MealDetailsPage({ params }) {
+  return <Meal params={params} />;
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import classes from './page.module.css'
 import Link from 'next/link'
 import MealsGrid from '@/components/(meals)/meals-grid'
 import { getMeals } from '@/lib/meals';
 
-export default async function MealsMain() {
+async function Meals () {
   const meals = await getMeals();
+  return <MealsGrid meals={meals}></MealsGrid>
+}
+
+export default function MealsMain() {
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +20,9 @@ export default async function MealsMain() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals}></MealsGrid>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals/>
+        </Suspense>
       </main>
     </>
   )
